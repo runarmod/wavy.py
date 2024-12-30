a9df0337541a16db50eedfef5bdaea5fdf9138d2
Runar Saur Modahl <runarmod@gmail.com>
2024-12-30 17:52:25 +0100

    refactor: simplify variable unpacking

--- b_spline.py
+++ b_spline.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def generate_estimated_coords(x, y, n: int = 100):
-    tck: tuple[np.ndarray, np.ndarray, int] = interpolate.splrep(x, y, s=0, k=3)  # type: ignore
+    t, c, k = interpolate.splrep(x, y, s=0, k=3)
     x_new = np.linspace(min(x), max(x), n)
-    y_fit = interpolate.BSpline(*tck)(x_new)
+    y_fit = interpolate.BSpline(t, c, k)(x_new)
     return [round(c, 4) for c in x_new], [round(c, 4) for c in y_fit]
